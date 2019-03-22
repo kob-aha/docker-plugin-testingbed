83c119e136a8d34fea659fc6f30241c8717e610d
Koby Aharon <kobyahron@gmail.com>
2019-03-22 21:08:05 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gradle Docker plugin Testingbed
+Sample project to test various Docker Gradle plugins.
+
+The project contains a simple greeting java application implemented with Spring boot and different branches for testing different Gradle plugins to build and run the sample application. 
---
+++ src/main/java/io/ka/testingbed/docker_plugin/Greeting.java
@@ -0,0 +1,20 @@
+package io.ka.testingbed.docker_plugin;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
